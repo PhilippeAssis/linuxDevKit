06b4d163202efa1888d56389de361253bd7f39f3
Philippe Assis <assis@philippeassis.com>
2017-01-30 02:54:25 -0200

    Update gitnpm.sh
    gittagnpm

--- gitnpm.sh
+++ gitnpm.sh
@@ -26,6 +26,28 @@ else
 	fi
 fi
 
+
+eval "$gitcommand && git push origin --tags"
+}
+
+gitnpmtag(){
+packagejson="$PWD/package.json"
+
+if [ -e $packagejson ]; then
+	packegeversion=$(cat package.json | grep -Po '(?<="version": ")[^"]*')
+	if [ -z "$1" ]; then
+		gitcommand="git tag -a $packegeversion -m $packegeversion"		
+	else
+		gitcommand="git tag -a $packegeversion -m $1"
+	fi
+else
+	if [ -z "$2" ]; then
+		gitcommand="git tag -a $1 -m $1"
+	else
+		gitcommand="git tag -a $1 -m \"$2\""
+	fi
+fi
+
 if [ -e $packagejson ]; then
 	eval "$gitcommand && git push origin --tags && npm publish"
 else
